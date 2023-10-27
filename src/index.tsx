f1ed5ce2a995587873acbb795fb8a22d253f6067
Alexei <avenge.now.r@mail.ru>
2023-10-27 18:05:07 +0300

    feat: add basic vite + react config

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/App';
+import './index.scss';
+import { setupStore } from './store';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={setupStore()}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
